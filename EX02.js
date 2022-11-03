b1ef7b0c678338e068dcf9d2be8434dc88e1a7e7
Adry <96891512+adriellyhuang@users.noreply.github.com>
2022-11-02 22:34:05 -0300

    Add files via upload

---
+++ EX02.js
@@ -0,0 +1,6 @@
+var velocidade = 59;
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+if (velocidade > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO`)
+}
+console.log(`Dirija sempre com sinto de segurança`)
